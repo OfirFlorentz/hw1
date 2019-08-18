9ad87880161d4ebcdde9baeddc9bd5c4bb96ee1b
OfirFlorentz <53431450+OfirFlorentz@users.noreply.github.com>
2019-08-18 21:46:14 +0300

    Update hw1dryq1.h

--- hw1dryq1.h
+++ hw1dryq1.h
@@ -4,6 +4,10 @@
 
 typedef struct list_t* List;
 
+void* listCreate(void* arr[], int n, void* copyElement(void*), void freeElement(void*));
+void listDestroy(List list, void freeElement(void*));
+void listDisplay(List list, void printElement(void*));
+
 
 #endif // ! LIST_H
 
